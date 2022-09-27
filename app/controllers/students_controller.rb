7db79042220141aaddb2a98780e5c2c854c00583
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-09-27 11:14:40 +0300

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
+    # students = if params[:name]
+    #   Student.find_by(id: params[:id], name: params[:name])
+    # else
+    # Student.all
+    # end
+    # render json: students
     students = Student.all
-    render json: students
+    render json: students 
   end
+  # def show 
+  #   student = Student.find(params[:id])
+  #   render json: student
+  # end
+def show 
+  student = Student.find_by(id: params[:id])
+  render json: student
+end
+
 
 end
